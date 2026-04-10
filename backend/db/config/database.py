15f935ef1f04364194bc1d20095f03931eb43fb9
Nimrit Singh Walia <104978201+0NSingh@users.noreply.github.com>
2026-04-11 02:04:59 +0530

    Switch to PostgreSQL database

--- backend/db/config/database.py
+++ backend/db/config/database.py
@@ -1,12 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@localhost:5432/calculator"
 )
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 Base = declarative_base()
 
